d7540481d238b878847827f97ff580a0557ee668
Olfuss Ratatosksson <ratatosksson@gmail.com>
2016-06-23 17:23:13 -0600

    Kicked ass
    Fixed form field entry code and duplicated to other fields.

--- login.rb
+++ login.rb
@@ -1,68 +1,107 @@
+require 'rubygems'
 require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :firefox
-
-driver.navigate.to "https://test.quomation.com/login.aspx"
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
-	element.send_keys "65789"
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
-	element.send_keys "jwilcox"
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
-	element.send_keys "jw"
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
-element = driver.find_element(:id, 'ctl00_loginViewChat_pnlLiveChatBox').click
-driver.quit();
+	driver.navigate.to "https://test.quomation.com/login.aspx"
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
+			element.send_keys "65789"
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
+			element.send_keys "jwilcox"
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
+			element.send_keys "jw"
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
+		element = driver.find_element(:id, 'ctl00_loginViewChat_pnlLiveChatBox').click
+	driver.quit();
 
 #Test PowerForms ID Card form and all fields
 
 driver2 = Selenium::WebDriver.for :firefox
-
-driver2.navigate.to "https://test.quomation.com/login.aspx"
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
-	element.send_keys "65789"
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
-	element.send_keys "jwilcox"
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
-	element.send_keys "jw"
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
-element = driver2.find_element(:id, 'btnPowerForms_text').click
-sleep(5)
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_dragPowerForms')
-element = driver2.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_btnDefaultIDCard').click
-driver2.quit();
+	driver2.navigate.to "https://test.quomation.com/login.aspx"
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
+			element.send_keys "65789"
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
+			element.send_keys "jwilcox"
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
+			element.send_keys "jw"
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
+		element = driver2.find_element(:id, 'btnPowerForms_text').click
+		sleep(5)
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_dragPowerForms')
+		element = driver2.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_btnDefaultIDCard').click
+	driver2.quit();
 
 driver3 = Selenium::WebDriver.for :firefox
+	driver3.navigate.to "https://test.quomation.com/PowerForms/InsuranceIDCard.aspx"
+		element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
+			element.send_keys "65789"
+		element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
+			element.send_keys "jwilcox"
+		element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
+			element.send_keys "jw"
+		element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
 
-driver3.navigate.to "https://test.quomation.com/PowerForms/InsuranceIDCard.aspx"
-element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
-	element.send_keys "65789"
-element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
-	element.send_keys "jwilcox"
-element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
-	element.send_keys "jw"
-element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
-element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_txtState').click
-	element.send_keys "RS"
-#element.send_keys "RS"
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_txtCompanyNumber').click
-#	element.send_keys "CompanySendKeySent"
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_txtCompany').click
-#	element.send_keys "TestCompanySent"
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_chkCommercial').click
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_chkPersonal').click
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_chkCommercial').click
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_chkPersonal').click
-#element = driver2.find_element(:id, 'ctl00_ContentPlaceHolder1_txtPolicyNumber').click
-#	element.send_keys "PolicyNumberKeySent"
+#This code has to be a little more robust to handle inputting into the fields on forms.	
 
+wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+	
+input = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_txtState')
+	element if element.displayed?
+}
+	input.send_keys("RS")
+
+input = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_txtCompanyNumber')
+	element if element.displayed?
+}
+	input.send_keys("CompanySendKeySent")
+
+input = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_txtCompany')
+	element if element.displayed?
+}
+	input.send_keys("TestCompanySent")
+
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkCommercial').click
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkPersonal').click
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkCommercial').click
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkPersonal').click
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkCommercial').click
+element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_chkPersonal').click
+
+input = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_txtPolicyNumber')
+	element if element.displayed?
+}
+	input.send_keys("PolicyNumberKeySent")
 
 # Test Effective Date and Expiration Date fields, however this needs to be fixed to test the dropdown feature selections as well.
 # Currently we are just sending a key value which is good, but we need to test both.
 
-#element = driver.find_element(:id, 'ctl00$ContentPlaceHolder1$mskEffectiveDate').click
-#	element.send_keys "06/14/2015"
-#element = driver.find_element(:id, 'ctl00$ContentPlaceHolder1$mskExpirationDate').click
-#	element.send_keys "06/14/2020"
+effdate = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_mskEffectiveDate')
+	element if element.displayed?
+}
+	effdate.click
+
+effdate = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_CalendarExtenderEffDate_today')
+	element if element.displayed?
+}
+	effdate.click
+
+expdate = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_mskExpirationDate')
+	element if element.displayed?
+}
+	expdate.click
+
+expdate = wait.until {
+	element = driver3.find_element(:id, 'ctl00_ContentPlaceHolder1_CalendarExtenderExpDate_today')
+	element if element.displayed?
+}
+	expdate.click
+	
 #element = driver.find_element(:id, 'ctl00$ContentPlaceHolder1$txtYear').click
 #	element.send_keys "2001"
 #element = driver.find_element(:id, 'ctl00$ContentPlaceHolder1$txtMake').click

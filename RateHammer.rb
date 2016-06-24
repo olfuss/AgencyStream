478325f22ee212d36c0729e07fb289bb9c454dde
Olfuss Ratatosksson <ratatosksson@gmail.com>
2016-06-24 13:14:32 -0600

    Added code to loop rate on XX browser instances.

--- RateHammer.rb
+++ RateHammer.rb
@@ -1,36 +1,56 @@
 require 'rubygems'
 require 'selenium-webdriver'
+require 'logger'
+
+logger = Logger.new(STDOUT)
+	logger.level=Logger::DEBUG
+		path = "RateHammerLog.txt"
+	
+begin
+  File.foreach(path) do |line|
+    unless line =~ /^(\w+) = (.*)$/
+      logger.error("Line in wrong format: #{line.chomp}")
+    end
+  end
 
 wait = Selenium::WebDriver::Wait.new(:timeout => 15)
 
-driver = Selenium::WebDriver.for :firefox
+i=0
+loop do 
+	i+=1
+		
+		driver = Selenium::WebDriver.for :firefox
+		driver.navigate.to "https://test.quomation.com/login.aspx"
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
+				element.send_keys("65789")
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
+				element.send_keys("jwilcox")
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
+				element.send_keys("jw")
+		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
+
+		quoteid = wait.until {
+			element = driver.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_txtSearch')
+			element if element.displayed?
+		}
+			quoteid.send_keys("1061943424")
+		
+		element = driver.find_element(:id, 'imgBtnSearch').click
+
+		uppnlclnt = wait.until {
+			element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_UpdatePanelClients')
+			element if element.displayed?
+		}
+			uppnlclnt.click
+
+		driver.navigate.to "https://test.quomation.com/HomeQuote.aspx?qid=1061943424"
+
+		load = wait.until {
+			element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_cmbCoAppRelation')
+			element if element.displayed?
+		}
+			element.send_keys(:control, '7')
 	
-driver.navigate.to "https://test.quomation.com/login.aspx"
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
-		element.send_keys("65789")
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
-		element.send_keys("jwilcox")
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_Password')
-		element.send_keys("jw")
-element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_btnLogin').click
-
-quoteid = wait.until {
-	element = driver.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_txtSearch')
-	element if element.displayed?
-}
-	quoteid.send_keys("1061943424")
-element = driver.find_element(:id, 'imgBtnSearch').click
-
-uppnlclnt = wait.until {
-	element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_UpdatePanelClients')
-	element if element.displayed?
-}
-	uppnlclnt.click
-
-driver.navigate.to "https://test.quomation.com/HomeQuote.aspx?qid=1061943424"
-
-load = wait.until {
-	element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_cmbCoAppRelation')
-	element if element.displayed?
-}
-	element.send_keys(:control, '7')
+	break if i==10
+end
+end

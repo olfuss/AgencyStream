f48972750e3d06a94c1dcfb565368cf12489fb1f
Olfuss Ratatosksson <ratatosksson@gmail.com>
2016-06-28 15:53:18 -0600

    Added some variables

--- RateHammer.rb
+++ RateHammer.rb
@@ -1,7 +1,18 @@
 require 'rubygems'
 require 'selenium-webdriver'
 
-wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+wait = Selenium::WebDriver::Wait.new(:timeout => 30)
+
+quote1 = 1061943424
+quote2 = 1248091520
+quote3 = 790520320
+quote4 = 1523928576
+quote5 = 1079073792
+quote6 = 1168224512
+quote7 = 1002762048
+quote8 = 1058327424
+quote9 = 1551535872
+quote10 = 977019776
 
 i=1
 loop do 
@@ -9,7 +20,7 @@ loop do
 		
 		driver = Selenium::WebDriver.for :firefox
 		print("Iteration #{i} Started")
-		driver.navigate.to "https://test.quomation.com/login.aspx"
+		driver.navigate.to "https://www.agencystream.com/login.aspx"
 		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_AgencyID')
 				element.send_keys("65789")
 		element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_Login_UserName')
@@ -22,7 +33,7 @@ loop do
 			element = driver.find_element(:id, 'ctl00_ContentPlaceHolderSidebar_txtSearch')
 			element if element.displayed?
 		}
-			quoteid.send_keys("1061943424")
+			quoteid.send_keys("#{quote1}")
 		
 		element = driver.find_element(:id, 'imgBtnSearch').click
 
@@ -32,7 +43,7 @@ loop do
 		}
 			uppnlclnt.click
 
-		driver.navigate.to "https://test.quomation.com/HomeQuote.aspx?qid=1061943424"
+		driver.navigate.to "https://test.quomation.com/HomeQuote.aspx?qid=#{quote1}"
 
 		load = wait.until {
 			element = driver.find_element(:id, 'ctl00_ContentPlaceHolder1_cmbCoAppRelation')
